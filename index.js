a1c76e230ffd8580679bc0028258098b0211eb85
Thomas Hooper <tom@stainlessvision.com>
2018-05-27 20:07:39 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,176 @@
+global.THREE = require('three/build/three');
+var CANNON = require('cannon');
+
+require('three/examples/js/controls/OrbitControls');
+
+var dt = 1/60, R = 0.2;
+
+const Main = function() {
+    this.initThree();
+    this.initCanon();
+    this.initScene();
+    window.addEventListener('resize', this.onResize.bind(this), false);
+    this.onResize();
+    this.animate();
+};
+
+Main.prototype.trefoil = function(a) {
+    return new THREE.Vector3(
+        Math.sin(a) + 2 * Math.sin(2 * a),
+        Math.cos(a) - 2 * Math.cos(2 * a),
+        -Math.sin(3 * a)
+    );
+};
+
+Main.prototype.initScene = function() {
+    this.scene = new THREE.Scene();
+    this.scene.add(this.camera);
+
+    var light = new THREE.PointLight( 0xff0000, 0.5, 100 );
+    light.position.set( -5, 5, 5 );
+    this.camera.add(light);
+
+    var ambient = new THREE.AmbientLight( 0xff0000, 0.5 );
+    this.camera.add(ambient);
+
+    var mat = new THREE.MeshLambertMaterial({
+        color: 0xff0000,
+        opacity: 0.5,
+        transparent: true
+    });
+    var tubeMat = new THREE.MeshLambertMaterial({
+        color: 0x333333
+    });
+
+    var count = 15;
+    var positions = [];
+    for (var i = 0; i < count; i++) {
+        var a = (i / count) * Math.PI * 2;
+        a += ((Math.PI * 2) / count) / 2;
+        var position = this.trefoil(a);
+        position.multiplyScalar(1/3);
+        positions.push(position);
+    }
+
+    var beadRadius = .3;
+
+    var bodies = positions.map(function(position, i) {
+        var shape = new CANNON.Sphere(beadRadius);
+        var body = new CANNON.Body({
+            mass: 1,
+            position: new CANNON.Vec3().copy(position)
+        });
+        body.addShape(shape);
+        this.world.addBody(body);
+        return body;
+    }.bind(this));
+
+    bodies.forEach(function(bodyA, i) {
+        var j = (i + 1) % bodies.length;
+        var bodyB = bodies[j];
+        var constraint = new CANNON.DistanceConstraint(
+            bodyA,
+            bodyB,
+            beadRadius * 1.8
+        );
+        constraint.collideConnected = false;
+        this.world.addConstraint(constraint);
+    }.bind(this));
+
+    var sphereGeom = new THREE.SphereGeometry(beadRadius,50,50);
+
+    this.spheres = bodies.map(function(bodies, i) {
+        var sphere = new THREE.Mesh(sphereGeom, mat);
+        sphere.body = bodies;
+        this.scene.add(sphere);
+        return sphere;
+    }.bind(this));
+
+    var tubeGeom = new THREE.CylinderGeometry(.01,.01,1);
+    tubeGeom.rotateX(Math.PI / 2);
+
+    this.tubes = this.spheres.map(function(sphere, i) {
+        var j = (i + 1) % this.spheres.length;
+        var nextSphere = this.spheres[j];
+        var tube = new THREE.Mesh(tubeGeom, tubeMat);
+        tube.sphereA = sphere;
+        tube.sphereB = nextSphere;
+        this.scene.add(tube);
+        return tube;
+    }.bind(this));
+
+    this.update();
+};
+
+Main.prototype.update = function() {
+    // console.log(this.world.bodies[0].position);
+
+    this.spheres.forEach(function(sphere) {
+        sphere.position.copy(sphere.body.position);
+        sphere.quaternion.copy(sphere.body.quaternion);
+    });
+
+    this.tubes.forEach(function(tube) {
+        var posA = tube.sphereA.position;
+        var posB = tube.sphereB.position;
+        var midpoint = posA.clone().lerp(posB, .5);
+        tube.position.copy(midpoint);
+        tube.lookAt(posA);
+        tube.scale.setZ(posA.distanceTo(posB));
+    });
+};
+
+Main.prototype.initCanon = function() {
+    this.world = new CANNON.World();
+    this.world.gravity.set(0,0,0);
+    this.world.broadphase = new CANNON.NaiveBroadphase();
+    this.world.solver.iterations = 10;
+};
+
+Main.prototype.initThree = function() {
+    var width = document.body.clientWidth;
+    var height = document.body.clientHeight;
+
+    this.renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true
+    });
+    this.renderer.setSize(width, height);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+
+    document.body.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(5, width / height, 0.1, 1000);
+    this.camera.position.set(0, 0, 30);
+    this.camera.up.set(0,-1,0);
+
+    this.cameraControls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+};
+
+Main.prototype.render = function() {
+    this.cameraControls.update();
+    this.renderer.render(this.scene, this.camera);
+};
+
+Main.prototype.animate = function() {
+    requestAnimationFrame(this.animate.bind(this));
+
+    this.world.step(dt);
+    // var t = world.time;
+    this.update();
+    this.render();
+};
+
+Main.prototype.setSize = function(width, height) {
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(width, height);
+};
+
+Main.prototype.onResize = function() {
+    var width = document.body.clientWidth;
+    var height = document.body.clientHeight;
+    this.setSize(width, height);
+};
+
+const main = new Main();
