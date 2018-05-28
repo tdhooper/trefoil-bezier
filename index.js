d542b8233d7e880f4020a11012edb7c08cd5e51a
Thomas Hooper <tom@stainlessvision.com>
2018-05-28 07:52:46 +0100

    Better alignment

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ Main.prototype.initScene = function() {
         var constraint = new CANNON.DistanceConstraint(
             bodyA,
             bodyB,
-            beadRadius * 1.8
+            beadRadius * 1.7
         );
         constraint.collideConnected = false;
         this.world.addConstraint(constraint);
@@ -129,7 +129,7 @@ Main.prototype.initScene = function() {
         material = angle2 ? angle2Mat : material;
 
         material = mat;
-        if ([14, 0, 1, 6, 7].indexOf(i) !== -1) {
+        if ([5, 9].indexOf(i) !== -1) {
             material = angle1Mat;
         }
 
@@ -186,29 +186,123 @@ Main.prototype.initScene = function() {
     var ang = (Math.PI * 2) / 3;
 
     angle1Plane.rotateZ(ang * 1);
-    angle1Plane.translateX(1);
+    // angle1Plane.translateX(1);
     angle1Plane.rotateX(Math.PI * .5);
 
     angle2Plane.rotateZ(ang * 2);
-    angle2Plane.translateX(1);
+    // angle2Plane.translateX(1);
     angle2Plane.rotateX(Math.PI * .5);
 
     angle3Plane.rotateZ(ang * 3);
-    angle3Plane.translateX(1);
+    // angle3Plane.translateX(1);
     angle3Plane.rotateX(Math.PI * .5);
 
     this.group.add(groundPlane);
     this.group.add(angle1Plane);
     this.group.add(angle2Plane);
     this.group.add(angle3Plane);
+
+    var m = new THREE.Mesh(
+        new THREE.SphereGeometry(.05),
+        new THREE.MeshLambertMaterial()
+    );
+    m.position.x = Math.sin((Math.PI / 6) * 2);
+    m.position.y = Math.cos((Math.PI / 6) * 2);
+    this.scene.add(m);
+};
+
+Main.prototype.orient = function(p, angle) {
+
+    var v = new THREE.Vector2(p.x, p.y);
+    var len = v.length();
+
+    p.x = Math.sin(angle) * len;
+    p.y = Math.cos(angle) * len;
+    // v.x
+
+    // var angle = v.angle();
+
+    // // console.log(p.y, p.x, 'k', angle);
+
+    // delta = this.mod(
+    //     targetAngle - angle + (Math.PI * 3),
+    //     Math.PI * 2
+    // ) - Math.PI;
+
+    // console.log(p.x, p.y, delta);
+
+    // var cs = Math.cos(-delta);
+    // var sn = Math.sin(-delta);
+
+    // var px = p.x * cs - p.y * sn;
+    // var py = p.x * sn + p.y * cs;
+
+    // p.x = px;
+    // p.y = py;
+
+    // return p;
 };
 
 Main.prototype.update = function() {
 
-    this.spheres[9].body.position.y = 0;
-    this.spheres[5].body.position.y = 0;
+    var TAU = Math.PI * 2;
+    var a = (Math.PI / 6);
 
-    // console.log(this.spheres[9].body.position.y);
+    var rotAndPlaneGroups = [
+        [0, 5, 10],
+        [1, 6, 11],
+        [2, 7, 12],
+        [3, 8, 13],
+        [4, 9, 14]
+    ];
+
+    var mirrorGroups = [
+        [0, 14],
+        [1, 13],
+        [2, 12],
+        [3, 11],
+        [4, 10],
+        [5, 9],
+        [6, 8],
+    ];
+
+    rotAndPlaneGroups.forEach(function(group) {
+        var rot = Math.PI * 2 / 3;
+        var a = this.spheres[group[0]].body.position;
+        var b = this.spheres[group[1]].body.position;
+        var c = this.spheres[group[2]].body.position;
+        var angle = new THREE.Vector2(c.y, c.x).angle();
+        this.orient(a, angle + rot);
+        this.orient(b, angle - rot);
+        a.z = c.z;
+        b.z = c.z;
+    }.bind(this));
+
+    mirrorGroups.forEach(function(group) {
+        var a = this.spheres[group[0]].body.position;
+        var b = this.spheres[group[1]].body.position;
+        a.x = -b.x;
+        a.y = b.y;
+        a.z = -b.z;
+    }.bind(this));
+
+    // this.orient(this.spheres[9].body.position, TAU / 4);
+    // this.orient(this.spheres[5].body.position, TAU / -4);
+
+
+    // this.orient(this.spheres[10].body.position, a * 1);
+    // this.orient(this.spheres[14].body.position, a * -5);
+
+    // this.orient(this.spheres[0].body.position, a * 5);
+    // this.orient(this.spheres[4].body.position, a * -1);
+
+    this.orient(this.spheres[2].body.position, a * 2);
+    this.orient(this.spheres[7].body.position, a * 6);
+    this.orient(this.spheres[12].body.position, a * -2);
+
+//     this.orient(this.spheres[12].body.position, (Math.PI / 6) * -2);
+
+// (Math.PI / 6) * 1 -.1
 
     this.outerSpheres.forEach(function(sphere) {
         sphere.body.position.z = 0;
@@ -300,7 +394,7 @@ Main.prototype.isStatic = function(lastPos, pos) {
     diff += Math.abs(pos.x - lastPos.x);
     diff += Math.abs(pos.y - lastPos.y);
     diff += Math.abs(pos.z - lastPos.z);
-    return diff < 0.00001;
+    return diff < 0.000001;
 };
 
 Main.prototype.animate = function() {
